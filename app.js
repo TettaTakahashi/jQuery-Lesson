14945b2f5d8a296e35a15e2ba1c335bc3c52f2e0
TettaTakahashi <x4tetta@gmail.com>
2024-05-24 03:33:27 +0000

    [Update]js7章

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 $(function(){
-  $('.box1').on('click',function(){
-    $('.box1').addClass('box-ect');
-  });
-  
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box-ect');
+  $('button').on('click', function(){
+    $('ul').children().css('color', 'red');
   });
 });
